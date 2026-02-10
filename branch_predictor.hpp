c58d35a32b88bf1d0754a3328d06d72253daa6ba
DrIronfist <reetsinha2006@gmail.com>
2026-02-10 14:46:26 -0600

    visualizing floorplan

--- branch_predictor.hpp
+++ branch_predictor.hpp
@@ -10,7 +10,7 @@
 #ifdef PREDICTOR
 using branch_predictor = PREDICTOR;
 #else
-//using branch_predictor = bimodal<>;
+using branch_predictor = bimodal<>;
 //using branch_predictor = gshare<>;
-using branch_predictor = tage<>;
+//using branch_predictor = tage<>;
 #endif
